809aadbc11ed6ea18bdacd4a4c285755d3dbd288
Nicolas Schmid <nicolas@breiten.ch>
2024-08-02 14:26:32 +0200

    refactor: put likelihood and em examples in same file

--- example_likelihood_em.py
+++ example_likelihood_em.py
@@ -70,4 +70,33 @@ plt.plot(x, fitted_m[1].pdf(x))
 plt.plot(x, generative_m[0].pdf(x), color='black', lw=1, ls='-.')
 plt.plot(x, generative_m[1].pdf(x), color='black', lw=1, ls='-.')
 
+
+# %%
+# Separate Example on likelihoods
+# Given two numbers (195 and 182) we want to find the likelihood of
+# these numbers given a normal distribution with different means
+# and standard deviations.
+
+lengths = np.array([195, 182])
+
+# lets create a grid of our two parameters
+mu = np.linspace(150, 250)
+sigma = np.linspace(0.1, 15)[::-1]
+
+mm, ss = np.meshgrid(mu, sigma)  # just broadcasted parameters
+
+
+# %%
+likelihood = stats.norm(mm, ss).pdf(
+    lengths[0]) * stats.norm(mm, ss).pdf(lengths[1])
+
+aspect = mm.max() / ss.max() / 3
+extent = [mm.min(), mm.max(), ss.min(), ss.max()]
+# extent = left right bottom top
+
+# %%
+plt.imshow(likelihood, cmap='Reds', aspect=aspect, extent=extent)
+plt.xlabel(r'$\mu$')
+plt.ylabel(r'$\sigma$')
+
 # %%
