89a99a983afb94d83a25a8fb7830b3175bb56054
Nicolas Schmid <nicolas@breiten.ch>
2025-05-04 08:47:17 +0200

    feat: improve settings for harpa

--- experiments/base_models/models_harpa.py
+++ experiments/base_models/models_harpa.py
@@ -1,5 +1,4 @@
 # %% Imports and Configuration
-
 import itertools
 
 import tqdm
@@ -10,23 +9,30 @@ from src.metrics import ClusterStatistics
 from src.runners import run_harpa
 
 config = {
-    "x(km)": (-1, 1),
-    "y(km)": (-1, 1),
-    "z(km)": (-1, 1),
-    "vel": {"P": 5.4, "S": 3.1},
-    "P_phase": True,
-    "S_phase": True,
-    "min_peak_pre_event": 5,
-    "min_peak_pre_event_s": 0,
-    "min_peak_pre_event_p": 0,
+    'x(km)': (-0.5, 0.5),
+    'y(km)': (-0.5, 0.5),
+    'z(km)': (-0.5, 0.5),
+    'vel': {'P': 5.4, 'S': 3.1},
+    'P_phase': True,
+    'S_phase': True,
+    'min_peak_pre_event': 4,
+    'min_peak_pre_event_s': 0,
+    'min_peak_pre_event_p': 0,
+    # 'dbscan_eps': 0.1,
+    # 'dbscan_min_samples': 4,
+    # 'lr': 0.01,
+    'lr_decay': 0.05,
+    # 'wasserstein_p': 2,
+    'max_time_residual':  0.01,
+    'epochs_before_decay': 10000,
+    'epochs_after_decay': 10000,
 }
 
-# %% Run GaMMA
 statistics = ClusterStatistics()
 
 
 ds = PhasePicksDataset(
-    root_dir='../../data/test_10',
+    root_dir='../../data/test_2',
     stations_file='stations.csv',
     file_mask='arrivals_*.csv',
     catalog_mask='catalog_*.csv',
@@ -35,11 +41,11 @@ ds = PhasePicksDataset(
 )
 
 
-for sample in tqdm.tqdm(itertools.islice(ds, len(ds))):
+for sample in tqdm.tqdm(itertools.islice(ds, 0, 2)):
     cat, labels_pred = run_harpa(sample.x, ds.stations, config)
+    y = sample.y.where(sample.y.map(sample.y.value_counts())
+                       > config['min_peak_pre_event'], -1)
+    statistics.add(y.to_numpy(), labels_pred)
 
-    statistics.add(sample.y.to_numpy(),
-                   labels_pred)
-
-print(f"HARPA ARI: {statistics.ari()}, Accuray: {statistics.accuracy()}, "
-      f"Precision: {statistics.precision()}, Recall: {statistics.recall()}")
+print(f'HARPA ARI: {statistics.ari()}, Accuray: {statistics.accuracy()}, '
+      f'Precision: {statistics.precision()}, Recall: {statistics.recall()}')
